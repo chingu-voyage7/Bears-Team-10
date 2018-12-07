d31cc45c08cf9f44e73cf33431ffad60b2076ea1
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-12-06 22:55:50 -0500

    added antd and dropdown menu

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FaPaw } from 'react-icons/fa';
+import { FaSignOutAlt } from 'react-icons/fa';
+import { FaSignInAlt } from 'react-icons/fa';
+import { Dropdown, Menu, Icon } from 'antd';
 
-const Header = () => (
-  <div>
-    <h1>Bairs</h1>
-    <p>Welcome!</p>
-  </div>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { user } = this.props;
 
+    const onClick = ({ key }) => {
+      // message.info(`Click on item ${key}`);
+    };
+
+    const menu = (
+      <Menu>
+        <Menu.Item key="1">
+          {user && user.loggedIn ? (
+            <FaSignOutAlt
+              title="logout"
+              onClick={() => console.log('leaving')}
+            />
+          ) : (
+            <FaSignInAlt title="login" onClick={() => console.log('here')} />
+          )}
+        </Menu.Item>
+        <Menu.Item key="2">2nd menu item</Menu.Item>
+        <Menu.Item key="3">3rd menu item</Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <div className="header-div">
+        <div className="logo">
+          <FaPaw />
+          <h1 className="header-title">Bairs</h1>
+        </div>
+        <p>
+          Welcome,{' '}
+          {user && user.loggedIn ? <span>{user.name}!</span> : 'please sign in'}
+        </p>
+        {/* dropdown */}
+        <Dropdown overlay={menu} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            Hover me, Click menu item <Icon type="down" />
+          </a>
+        </Dropdown>
+      </div>
+    );
+  }
+}
 export default Header;
--- frontend/src/Components/Header/Header.test.js
+++ frontend/src/Components/Header/Header.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 
-test('can render the logo', () => {
-  const container = document.createElement('div');
-  ReactDOM.render(<Header />, container);
+describe('Header component', () => {
+  test('can render the logo', () => {
+    const container = document.createElement('div');
+    ReactDOM.render(<Header />, container);
 
-  expect(container.textContent).toContain('Bairs');
+    expect(container.textContent).toContain('Bairs');
+  });
+
+  test('has logo', () => {
+    const container = document.createElement('div');
+    ReactDOM.render(<Header />, container);
+
+    expect(container);
+  });
 });
