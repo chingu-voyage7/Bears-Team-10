425000f29d958ac3e4120994336108b5b7ed2843
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-12-07 01:03:58 -0500

    created tests for Header

--- frontend/src/Components/Header/Header.test.js
+++ frontend/src/Components/Header/Header.test.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import Header from './Header';
+import { FaPaw } from 'react-icons/fa';
 
 describe('Header component', () => {
   test('can render the logo', () => {
-    const container = document.createElement('div');
-    ReactDOM.render(<Header />, container);
-
-    expect(container.textContent).toContain('Bairs');
+    const container = shallow(<Header />);
+    expect(container.find('.logo')).toBeDefined();
   });
 
-  test('has logo', () => {
-    const container = document.createElement('div');
-    ReactDOM.render(<Header />, container);
+  test('has h1 elem', () => {
+    const container = shallow(<Header />);
+    const welcome = <h1 className="header-title">Bairs</h1>;
+    expect(container.contains(welcome)).toEqual(true);
+  });
 
-    expect(container);
+  test('p tag with welcome message', () => {
+    const props = {
+      loggedIn: false
+    };
+    const container = mount(<Header user={props} />);
+    const welcome = <p>Welcome, please sign in</p>;
+    expect(container.prop('user')).toEqual({ loggedIn: false });
+    expect(container.contains(welcome)).toEqual(true);
+  });
+
+  test('can welcome the user if logged in', () => {
+    const props = {
+      loggedIn: true,
+      name: 'Carl'
+    };
+    const container = mount(<Header user={props} />);
+    expect(container.text()).toContain('Carl');
   });
 });
