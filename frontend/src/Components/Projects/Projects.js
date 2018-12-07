f0d51eadba6ff761867149837febf462dc9386fb
leighd2008 <leighd2008@gmail.com>
2018-12-07 12:50:49 -0500

    added click to show / hide posts

--- frontend/src/Components/Projects/Projects.js
+++ frontend/src/Components/Projects/Projects.js
@@ -3,30 +3,44 @@ import "./Projects.css";
 import ProjectPosts from "../ProjectPosts/ProjectPosts"
 
 class Projects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      postsVisible: false
+    }
+  }
+
+  onClick() {
+    this.setState(prevState => ({ postsVisible: !prevState.postsVisible }));
+  }
 
   render() {
 
     return (
       <div className="project">
-        <div className="projectText" >
+        <div className="projectText" onClick={() => this.onClick()}>
           <div className="projectTitle" >
             <span > {this.props.title} </span>
           </div>
           <div className="projectDescription" >
             <span > {this.props.desc} </span>
           </div>
-          <div class="projectList" > {
-            this.props.posts.map((post, i) => {
-              return (
-                <ProjectPosts key={i}
-                  id={this.props.posts[i].id}
-                  name={this.props.posts[i].name}
-                  message={this.props.posts[i].message}
-                />
-              );
-            })
+          Click to show / hide posts
+          {
+            this.state.postsVisible
+              ? <div class="postList" > {
+                this.props.posts.map((post, i) => {
+                  return (
+                    <ProjectPosts key={i}
+                      id={this.props.posts[i].id}
+                      name={this.props.posts[i].name}
+                      message={this.props.posts[i].message}
+                    />);
+                })
+              }
+              </div>
+              : null
           }
-          </div>
         </div>
       </div>
     );
