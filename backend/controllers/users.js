67ca6818ea4cd37a55326231bda6d2db3e75390a
willybeans <willwedmedyk@gmail.com>
2018-12-12 22:27:56 -0500

    added user routes/table/db queries
    Added user update route in routes/users.js and controllers/users.js. Also added more columns to the user table, and added a SQL query in db/index for updating the users profile items.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,30 @@
+const db = require("../config/db");
 
-function hello (ctx) {
-  let user = ctx.request.query.user
-  ctx.ok({ user })
+async function hello(ctx) {
+  let user = ctx.state.user;
+  ctx.body = "Hello";
+  ctx.send(200, {
+    user
+  });
+}
+
+async function updateUserProfile(ctx) {
+  const updatedUser = await db.updateUserProfile(
+    ctx.request.body.profile_picture,
+    ctx.request.body.display_name,
+    ctx.request.body.bio,
+    ctx.request.body.interests,
+    ctx.request.body.github,
+    ctx.state.user.id
+  );
+  console.log(updatedUser);
+  ctx.send(201, {
+    message: "Update Succesful!",
+    user: updatedUser
+  });
 }
 
 module.exports = {
-  hello
-}
+  hello,
+  updateUserProfile
+};
