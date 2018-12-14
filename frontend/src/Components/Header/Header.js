32366e1e092eec8f2dcf171852155a4aba06b3f4
leighd2008 <leighd2008@gmail.com>
2018-12-14 12:19:57 -0500

    fixed duplication in header.js from wrong full merge

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -54,15 +54,4 @@ class Header extends Component {
   }
 }
 
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const Header = () => (
-  <div>
-    <h1>Bairs</h1>
-    <p>Welcome!</p>
-  </div>
-);
-
-
 export default Header;
