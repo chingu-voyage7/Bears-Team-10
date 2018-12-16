1d2faf2c2ae1ccbf27e5602f2716c74e2301d9ff
leighd2008 <leighd2008@gmail.com>
2018-12-16 14:36:06 -0500

    changed to eslint airbnb

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Chingu Voyage-7 Bears Team 10 lets build something Awesome!</p>
+    </header>
+  </div>
+);
+
+export default App;
