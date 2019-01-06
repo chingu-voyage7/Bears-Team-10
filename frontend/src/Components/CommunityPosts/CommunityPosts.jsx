2979bad3f2d8c6d8a172ea818d75dc113b970ba4
leighd2008 <leighd2008@gmail.com>
2019-01-06 11:34:12 -0500

    'Merge' CommunityPosts from Projects_Posts branch

---
+++ frontend/src/Components/CommunityPosts/CommunityPosts.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import ProjectPosts from '../ProjectPosts/ProjectPosts';
+import NewPost from '../NewPost/NewPost';
+import './CommunityPosts.css';
+import { posts } from '../../posts';
+
+const CommunityPosts = () => (
+  <div className="contentContainer">
+    <div className="contentAndTitles">
+      <div
+        className="projectText"
+        role="presentation"
+        // onClick={() => this.props.history.push('projects', index)}
+      >
+        <div className="projectTitle">
+          <span>{posts[0].title}</span>
+        </div>
+        <div className="projectDescription">
+          <span>{posts[0].desc}</span>
+        </div>
+        Click to hide posts
+      </div>
+      <div className="postList">
+        {posts[0].posts.map(post => (
+          <ProjectPosts
+            key={post.id}
+            id={post.id}
+            name={post.name}
+            message={post.message}
+            date={post.date}
+          />
+        ))}
+        <NewPost />
+      </div>
+    </div>
+  </div>
+);
+
+CommunityPosts.propTypes = {
+  // onRouteChange: PropTypes.func.isRequired,
+  // index: PropTypes.string.isRequired,
+};
+
+export default CommunityPosts;
