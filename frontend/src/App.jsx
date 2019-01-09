8e59c1476d13f0e7b076ed8f11352fded9578bec
leighd2008 <leighd2008@gmail.com>
2019-01-09 10:54:27 -0500

    fixing switch for community projects / posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import './App.css';
 import { fetchUser } from './redux/auth';
@@ -12,10 +12,9 @@ import Login from './Components/Authentication/Login';
 import CreateProject from './Components/Projects/CreateProject';
 import Profile from './Components/UserProfile/Profile';
 
-import CommunityProjects from './Component/CommunityProjects/CommunityProjects';
+import CommunityProjects from './Components/CommunityProjects/CommunityProjects';
 import Dashboard from './Components/Dashboard/Dashboard';
 import CommunityPosts from './Components/CommunityPosts/CommunityPosts';
-
 
 class App extends Component {
   constructor() {
@@ -42,26 +41,24 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div className="App">
-
-            <Header 
+            <Header
               displayPosts={this.state.displayPosts}
               onClickPosts={this.onClickPosts}
             />
-            <Dashboard />
-            <Switch>
+            <Dashboard
+              displayPosts={this.state.displayPosts}
+              onClickPosts={this.onClickPosts}
+            />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/create-project" component={CreateProject} />
             <Route exact path="/profile" component={Profile} />
-            <Route exact path="/communityprojects" component={CommunityProjects}
-            </Switch>
             <Route
               exact
               path="/community-projects"
               component={CommunityProjects}
             />
             <Route exact path="/community-posts" component={CommunityPosts} />
-
           </div>
         </BrowserRouter>
       </div>
---
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+
+// import { FaPlus } from 'react-icons/fa';
+import CommunityProjects from '../CommunityProjects/CommunityProjects';
+import CommunityPosts from '../CommunityPosts/CommunityPosts';
+
+class Dashboard extends Component {
+  state = {};
+
+  render() {
+    const { displayPosts, allProjects } = this.props;
+    return (
+      <div className="Dashboard">
+        <div>
+          <Link to="/create-project">New Project</Link>
+        </div>
+        <div>
+          <Link to="/profile">Profile</Link>
+        </div>
+        <div>
+          <Link to="/communityprojects">Community Projects</Link>
+        </div>
+        <div>
+          <Link to="/">Your Projects</Link>
+        </div>
+        {(() => {
+          switch (displayPosts) {
+            case true:
+              return (
+                <CommunityPosts
+                  displayPosts={displayPosts}
+                  onClickPosts={onClickPosts}
+                />
+              );
+            default:
+              return (
+                <CommunityProjects
+                  allProjects={allProjects}
+                  displayPosts={displayPosts}
+                  onClickPosts={onClickPosts}
+                />
+              );
+          }
+        })()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user,
+    allProjects: state.projects.allProjects,
+  };
+}
+
+Dashboard.propTypes = {
+  // clearProjectData: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  allProjects: PropTypes.object.isRequired,
+  displayPosts: PropTypes.bool.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  // history: PropTypes.object.isRequired,
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {}
+  )(Dashboard)
+);
