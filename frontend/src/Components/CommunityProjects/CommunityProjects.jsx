1c7558a151151d74917caaec0daf527136e908ad
willybeans <willwedmedyk@gmail.com>
2019-01-19 18:16:43 -0500

    cleaning commented code

--- frontend/src/Components/CommunityProjects/CommunityProjects.jsx
+++ frontend/src/Components/CommunityProjects/CommunityProjects.jsx
@@ -5,43 +5,7 @@ import PropTypes from 'prop-types';
 import Projects from './Projects';
 import './CommunityProjects.css';
 
-// for calling this component and the CommunityPosts (switching between
-// them with onClick) use:
-// {(() => {
-//   switch (displayPosts) {
-//     case true:
-//       return (
-//         <CommunityPosts
-//           displayPosts={displayPosts}
-//           onClickPosts={onClickPosts}
-//           projectId={projectId}
-//         />
-//       );
-//     default:
-//       return (
-//         <CommunityProjects
-//           allProjects={allProjects}
-//           displayPosts={displayPosts}
-//           onClickPosts={onClickPosts}
-//           user={user}
-//         />
-//       );
-//   }
-// })()}
-// in Dashboard Component
-//
-//  add:     allProjects: state.projects.allProjects
-//  to mapStateToProps
-//  in Dashboard Component
-//
-// add:
-// displayPosts={this.state.displayPosts}
-// onClickPosts={this.onClickPosts}
-// projectId={this.state.projectId}
-// as props for Dashboard Component in App.jsx
-
 const CommunityProjects = ({
-  // user,
   allProjects,
 }) => (
   <div className="contentContainer">
@@ -63,10 +27,7 @@ const CommunityProjects = ({
 );
 
 CommunityProjects.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   allProjects: PropTypes.array.isRequired,
-  // eslint-disable-next-line react/forbid-prop-types
-  // user: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
