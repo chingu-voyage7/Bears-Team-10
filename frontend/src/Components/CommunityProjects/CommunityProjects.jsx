6a1ecb28d0a1026a1510851083ccadb79f8f9136
willybeans <willwedmedyk@gmail.com>
2019-01-19 19:49:42 -0500

    added ANTD library styling
    removed project component from display

--- frontend/src/Components/CommunityProjects/CommunityProjects.jsx
+++ frontend/src/Components/CommunityProjects/CommunityProjects.jsx
@@ -2,25 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Projects from './Projects';
 import './CommunityProjects.css';
+import { Card } from 'antd';
+
+const gridStyle = {
+  width: '98vw',
+  textAlign: 'left',
+  margin: '2px'
+};
 
 const CommunityProjects = ({
   allProjects,
 }) => (
   <div className="contentContainer">
     <div className="contentAndTitles">
-      <div className="projectsTitle">
-        <span> Project List </span>
+      <div className="allCardsTitle">
+        <h1>Community Projects</h1>
       </div>
       <div className="projectList">
-        {allProjects.length
-          ? allProjects.map(project => (
-              <Link to={`/project/${project.project_id}`}>
-                <Projects project_id={project.project_id} />
-              </Link>
-            ))
-          : 'Nothing here'}
+          {
+            allProjects.length
+            ? allProjects.map(project => (
+              <div className="cardItem">
+                            <Link to={`/project/${project.project_id}`} style={{ textDecoration: 'none', color: 'black' }}>
+              <Card title={(() => (
+                <div className="cardTitle">
+                <div><h2>{project.project_title.toUpperCase()}</h2></div>
+                <div>{project.creation_timestamp.substring(0,10)}</div>
+                </div>
+              ))()}
+              bordered={true}
+              style={gridStyle}>
+
+                <div className="cardDescription">{project.project_description}</div>
+
+            </Card>
+                        </Link>
+            </div>
+              ))
+            : 'Nothing here'}
+
       </div>
     </div>
   </div>
